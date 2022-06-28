d98df1db5c1f566695a358634d7d5b5346f57773
Raunak0604 <raunakmondal0604@gmail.com>
2022-06-28 15:58:37 +0530

    File added

--- 03_Variables_&_DataTypes.js
+++ 03_Variables_&_DataTypes.js
@@ -9,16 +9,16 @@ console.log("The sum is : ", number1 + number2);
 // 4. Data types in JavaScript
 // Numbers
 var num1 = 455;
-var num2 = 56.76;
+num2 = 56.76;
 console.log(num1, num2);
 
 // String
 var str1 = "This is a string";
-var str2 = 'This is also a string';
+str2 = 'This is also a string';
 console.log(str1, str2);
 
 // Objects
-var marks = {
+marks = {
     rabbit: 34,
     souvik: 78,
     yashwant: 99.977,
@@ -27,7 +27,7 @@ console.log(marks);
 
 // Booleans
 var a = true;
-var b = false;
+b = false;
 console.log(a, b);
 
 // Undefine
